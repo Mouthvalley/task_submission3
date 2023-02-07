a1b23679ea055e759e95b051456f21fa3d1d3ffe
Mouthvalley <yamatotaniguchi1107@gmail.com>
2023-02-07 05:26:02 +0000

    [A]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,24 +17,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    # user_id = params[:id].to_i
-    # unless user_id == current_user.id
-    # redirect_to user_path(current_user.id)
-    # end
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
+    if user.update(user_params)
     flash[:notice] = "You have updated user successfully."
-    redirect_to user_path(@user.id)
+    redirect_to user_path(user.id)
     else
       render :edit
     end
-    # user_id = params[:id].to_i
-    # unless user_id == current_user.id
-    #   redirect_to user_path
-    # end
   end
 
 
